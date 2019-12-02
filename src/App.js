384bd85ee8d6096a024868fbb31aca525e75fb02
mvasylykiv <mvasylykiv@griddynamics.com>
2019-12-02 14:02:07 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Particles from 'react-particles-js';
 
-import SmallChatWindow from './components/SmallChatWindow';
+// import SmallChatWindow from './components/SmallChatWindow';
 
 import './App.scss';
 
