a3f04baa440698c1e0e4338b4658190a3575448e
mvasylykiv <mvasylykiv@griddynamics.com>
2019-12-02 17:26:41 +0200

    styles done

--- src/components/SmallChatWindow/SmallChatWindow.js
+++ src/components/SmallChatWindow/SmallChatWindow.js
@@ -1,7 +1,54 @@
 import React from 'react';
 
-const SmallChatWindow = () => (
-  <div>YES</div>
-);
+import MinimizeOutlinedIcon from '@material-ui/icons/MinimizeOutlined';
+import SettingsApplicationsOutlinedIcon from '@material-ui/icons/SettingsApplicationsOutlined';
+import PeopleAltOutlinedIcon from '@material-ui/icons/PeopleAltOutlined';
+import TextField from '@material-ui/core/TextField';
+import SendIcon from '@material-ui/icons/Send';
+import IconButton from '@material-ui/core/IconButton';
+
+import './SmallChatWindow.scss';
+
+const CN = 'small-window';
+
+class SmallChatWindow extends React.Component {
+  render() {
+    const chatTitle = 'Chat title';
+
+    return (
+      <div className={CN}>
+        <div className={`${CN}__title-wrapper`}>
+        <IconButton
+            type="submit"
+            className={`${CN}__btn`}
+            label="text"
+          >
+            <MinimizeOutlinedIcon />
+          </IconButton>
+          <span>{chatTitle}</span>
+          <IconButton
+            type="submit"
+            className={`${CN}__btn`}
+            label="text"
+          >
+            <PeopleAltOutlinedIcon />
+          </IconButton>
+          <IconButton
+            type="submit"
+            className={`${CN}__btn`}
+            label="text"
+          >
+            <SettingsApplicationsOutlinedIcon />
+          </IconButton>
+        </div>
+        <div className={`${CN}__dialogue-wrapper`} />
+        <div className={`${CN}__input-wrapper`}>
+          <TextField className={`${CN}__dialogue-input`} id="outlined-basic" label="Your message" variant="outlined" />
+          <IconButton className={`${CN}__send-btn`}><SendIcon fontSize="large" /></IconButton>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default SmallChatWindow;
